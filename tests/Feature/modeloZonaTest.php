c8e4f13d452ca54c9c725ac79f65d96ef41e9586
cristian <cagudelovillegas@gmail.com>
2017-10-21 22:42:57 -0500

    Creando test de zona

---
+++ tests/Feature/modeloZonaTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class modeloZonaTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testZona()
+	{
+    // Make call to application...
+
+    	$this->assertDatabaseHas('zona', [
+	    	'idzona' => 14,
+	        'zona' => 'meta',
+	        'ts' => '2017-10-21 20:42:00',
+	        'idusuario' => 30,
+
+    	]);
+    }
+
+}
