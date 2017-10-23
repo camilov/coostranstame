d68328a730e4ef83935cf3f1e1e5ddc5c9ccfda1
cristian <cagudelovillegas@gmail.com>
2017-10-22 23:24:15 -0500

    test de tercero satisfactorio

--- tests/Feature/modeloTerceroTest.php
+++ tests/Feature/modeloTerceroTest.php
@@ -18,4 +18,22 @@ class modeloTerceroTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function testTercero()
+	{
+    // Make call to application...
+
+    	$this->assertDatabaseMissing('tercero', [
+	    	'idtercero' =>19,
+	        'cedula' => 12313,
+	        'ciudad' => 'Pereira',
+	        'nombre' => 'Carla',
+	        'apellido'=>'villegas',
+	        'idusuario'=>'30',
+	        'nit'=>12123,
+	        'celular'=>314254535,
+	        'direccion'=>'cra 6#3-22',
+	        'correo'=>'carla@gmail.com',
+    	]);
+    }
 }
