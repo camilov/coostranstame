629e9e0f98c92d988cba25d5a02f240566411014
cristian <cagudelovillegas@gmail.com>
2017-10-22 23:28:57 -0500

    test de guia manual satisfactorio

--- tests/Feature/modeloGuiamanualTest.php
+++ tests/Feature/modeloGuiamanualTest.php
@@ -18,4 +18,24 @@ class modeloGuiamanualTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function testGuiaManual()
+	{
+    // Make call to application...
+
+    	$this->assertDatabaseMissing('guia_manual', [
+	    	'idguia_manual'=>13,
+	    	'remision'=>1212,
+	    	'valor_declarado'=>10022,
+	    	'flete'=>132,
+	    	'valor_neto'=>132,
+	    	'idtercero'=>8,
+	    	'seguro'=>1231,
+	    	'tipo_remesa'=>'remesa',
+	    	'forma_pago'=>'contado',
+	    	'origen'=>'bogota',
+	    	'destino'=>'pereira',
+
+    	]);
+    }
 }
